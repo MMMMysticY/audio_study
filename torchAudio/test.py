67c3a819f2d308a19845c3d8f19858ce304957cd
MMMMysitcY <810908676@qq.com>
2022-04-28 20:37:20 +0800

    torchaudio_advanced_usages

--- torchAudio/test.py
+++ torchAudio/test.py
@@ -2,3 +2,11 @@ import torch
 import torchaudio
 print(torch.__version__)
 print(torchaudio.__version__)
+data_root_path = '/home/wy/audio_study/data/study_data/'
+SPEECH_FILE = data_root_path + "speech.wav"
+waveform, sample_rate = torchaudio.load(SPEECH_FILE)
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+waveform = waveform.to(device)
+print(waveform)
+# import sys
+# print(sys.path)
